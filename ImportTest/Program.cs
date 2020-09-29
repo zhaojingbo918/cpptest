66124ac783f5ef26969b1aa93e0247a90f325c7f
jingbo <jingbo@nolovr.com>
2020-09-29 10:15:31 +0800

    实现功能

---
+++ ImportTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var buf = new byte[300];
+            getHelloWorld(buf);
+            Console.WriteLine(System.Text.Encoding.ASCII.GetString(buf));
+
+            Console.ReadLine();
+        }
+
+        [DllImport("ExportTest2.dll")]
+        public static extern void getHelloWorld(byte[] buf);
+    }
+}
